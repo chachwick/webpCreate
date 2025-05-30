fcdd77d2a7ed1a3bf41cdbf5e334bba74c11cade
chachwick <41742797+chachwick@users.noreply.github.com>
2025-05-30 19:36:46 -0400

    Update update-webpCreate.sh
    Update to allow for both Intel and Apple silicon to update man pages

--- update-webpCreate.sh
+++ update-webpCreate.sh
@@ -12,7 +12,8 @@ curl -fsSL https://raw.githubusercontent.com/chachwick/webpCreate/main/update-we
 chmod +x "$HOME/scripts/update-webpCreate"
 
 echo "📄 Updating man page…"
-MAN_DIR="/usr/local/share/man/man1"
+BREW_PREFIX=$(brew --prefix)
+MAN_DIR="$BREW_PREFIX/share/man/man1"
 mkdir -p "$MAN_DIR"
 curl -fsSL https://raw.githubusercontent.com/chachwick/webpCreate/main/man/webpCreate.1 \
   -o "$MAN_DIR/webpCreate.1"
